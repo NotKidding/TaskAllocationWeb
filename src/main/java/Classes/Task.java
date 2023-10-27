00dfc6325ffb8125b14ccd8e59e3bfd24a753ee9
NotKidding <62922466+NotKidding@users.noreply.github.com>
2023-10-28 03:25:26 +0530

    Class Task added.

---
+++ src/main/java/Classes/Task.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+
+/**
+ *
+ * @author HP
+ */
+
+//This represent tasks
+public class Task {
+    private String name;
+    private int priority;
+    private int requiredSkill;
+    
+    public Task(String name, int priority, int requiredSkill){
+    this.name = name;
+    this.priority = priority;
+    this.requiredSkill = requiredSkill;
+    }
+    //Create getters and setters for these properties
+}
