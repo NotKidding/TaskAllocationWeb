622fb801034b8743ccb45a3c7e6367eb033adf2f
NotKidding <62922466+NotKidding@users.noreply.github.com>
2023-10-28 04:51:59 +0530

    Class TeamMember modified.

--- src/main/java/Classes/TeamMember.java
+++ src/main/java/Classes/TeamMember.java
@@ -11,10 +11,49 @@ package Classes;
 public class TeamMember {
     private String name;
     private int skillLevel;
+    private int currentWorkLoad; //Added property to track workload
     
     public TeamMember(String name, int skillLevel){
         this.name = name;
         this.skillLevel = skillLevel;
+        this.currentWorkLoad = 0; //start workload from 0
     }
-    //Create getters and setters
+    
+    //Getters
+    public String getName(){
+        return name;
+    }
+    public int getSkillLevel(){
+        return currentWorkload;
+    }
+    
+    //Setters
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setSkillLevel(int skillLevel){
+        this.skillLevel = skillLevel;
+    }
+    public void setCurrentWorkload(int currentWorkLoad){
+        this.currentWorkLoad = currentWorkload;
+    }
+    
+    //Additional method to add work
+    public void addWork(int workAmount){
+        currentWorklaod += workAmount;
+    }
+    
+    //Additional method to check if the team member is overloaded
+    public boolean is Overloaded(int workloadLimit){
+        return currentWorkload > workloadLimit;
+    }
+    
+    @Override
+    public String toString(){
+        return "TeamMember{" +
+                "name='" + name + '\'' +
+                ", skillLevel=" + skillLevel +
+                ", currentWorkload=" + currentWorkload +
+                '}';
+    }
 }
